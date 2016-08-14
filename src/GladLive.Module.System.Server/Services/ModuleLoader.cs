4da8a31a1abc19e34e8ca056db25660ba2bb8337
Andrew Blakely <andrew.blakely@ymail.com>
2016-08-13 23:09:09 -0500

    Fixed Missing Unreferenced Assembly Loading

--- src/GladLive.Module.System.Server/Services/ModuleLoader.cs
+++ src/GladLive.Module.System.Server/Services/ModuleLoader.cs
@@ -32,7 +32,8 @@ namespace GladLive.Module.System.Server
 		private static Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
 		{
 			Assembly ass = null;
-			string pathName = args.Name.Contains(".dll") ? args.Name : $"{args.Name}.dll";
+			string nonFullName = args.Name.Split(',').FirstOrDefault();
+			string pathName = nonFullName.Contains(".dll") ? nonFullName : $"{nonFullName}.dll";
 
 			if (File.Exists(pathName))
 				ass = Assembly.Load(File.ReadAllBytes(pathName));
@@ -82,6 +83,7 @@ namespace GladLive.Module.System.Server
 					//Check the appdomain and see if we can find an assembly loaded with that name already
 					if (AppDomain.CurrentDomain.GetAssemblies().Where(a => a.GetName().FullName == name.FullName).Count() == 0)
 					{
+						Console.WriteLine($"Trying to load: {name.FullName} Short Name: {name.Name}");
 						try
 						{
 							//Probably will fail
